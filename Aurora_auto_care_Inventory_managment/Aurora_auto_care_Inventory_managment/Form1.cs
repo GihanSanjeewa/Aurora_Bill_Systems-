fa69abc295b4a115bb50311909079f210cbfd96b
GihanSanjeewa <gihansanjeewa.m@gmail.com>
2023-05-19 19:35:27 +0530

    starting design

---
+++ Aurora_auto_care_Inventory_managment/Aurora_auto_care_Inventory_managment/Form1.cs
@@ -0,0 +1,10 @@
+namespace Aurora_auto_care_Inventory_managment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
