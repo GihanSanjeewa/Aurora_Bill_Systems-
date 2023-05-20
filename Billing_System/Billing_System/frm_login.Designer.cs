671d9685dedd8b8cb5796bca69285ccf615d2acc
GihanSanjeewa <gihansanjeewa.m@gmail.com>
2023-05-21 03:08:00 +0530

    add login and dashbord forms

--- Billing_System/Billing_System/frm_login.Designer.cs
+++ Billing_System/Billing_System/frm_login.Designer.cs
@@ -28,38 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.guna2CircleButton1 = new Guna.UI2.WinForms.Guna2CircleButton();
-            this.SuspendLayout();
-            // 
-            // guna2CircleButton1
-            // 
-            this.guna2CircleButton1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
-            this.guna2CircleButton1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
-            this.guna2CircleButton1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
-            this.guna2CircleButton1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
-            this.guna2CircleButton1.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.guna2CircleButton1.ForeColor = System.Drawing.Color.White;
-            this.guna2CircleButton1.Location = new System.Drawing.Point(391, 145);
-            this.guna2CircleButton1.Name = "guna2CircleButton1";
-            this.guna2CircleButton1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
-            this.guna2CircleButton1.Size = new System.Drawing.Size(148, 148);
-            this.guna2CircleButton1.TabIndex = 0;
-            this.guna2CircleButton1.Text = "guna2CircleButton1";
-            // 
-            // frm_login
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(891, 473);
-            this.Controls.Add(this.guna2CircleButton1);
-            this.Name = "frm_login";
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Text = "frm_login";
-            this.ResumeLayout(false);
-
         }
 
         #endregion
-
-        private Guna.UI2.WinForms.Guna2CircleButton guna2CircleButton1;
     }
 }
