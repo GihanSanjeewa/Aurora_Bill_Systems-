58408ed92bd777a4bd2c10f23294f260625231a3
GihanSanjeewa <gihansanjeewa.m@gmail.com>
2023-05-21 15:17:10 +0530

    g

---
+++ Billing_System/Billing_System/frm_motoBicycle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Billing_System
+{
+    public partial class frm_motoBicycle : Form
+    {
+        public frm_motoBicycle()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_process_Click(object sender, EventArgs e)
+        {
+            frm_sconditionReport sc =   new frm_sconditionReport();
+            this.Hide();
+            sc.Show();
+        }
+    }
+}
