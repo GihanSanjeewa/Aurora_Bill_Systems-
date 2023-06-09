27912e6b3d4a1b02cb3e2fe94fce863de46d8484
gamikapunsisi <rmgpramanayaka@gmail.com>
2023-06-10 00:29:33 +0530

    add purshase form

---
+++ Billing_System/Billing_System/frm_purchase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Billing_System
+{
+    public partial class frm_purchase : Form
+    {
+        public frm_purchase()
+        {
+            InitializeComponent();
+        }
+
+        private void item_purchPrice_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void purchase_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
