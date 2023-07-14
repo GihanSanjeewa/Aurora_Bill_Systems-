421fc9be792f35bb7cb5f298b12b010ce1aaba74
Gihan Sanjeewa <123794603+GihanSanjeewa@users.noreply.github.com>
2023-07-14 21:30:40 +0530

    Revert "Revert "ss""

--- Billing_System/Billing_System/motobicycleJobCard.cs
+++ Billing_System/Billing_System/motobicycleJobCard.cs
@@ -79,6 +79,11 @@ namespace Billing_System
             print(this.panel1);
         }
 
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
         //private void label1_Click(object sender, EventArgs e)
         //{
 
