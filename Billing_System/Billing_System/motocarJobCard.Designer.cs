1b0a250f91377616e6262a0a9ceea8264a963b29
GihanSanjeewa <gihansanjeewa.m@gmail.com>
2023-07-21 23:40:47 +0530

    jobcard car completed

--- Billing_System/Billing_System/motocarJobCard.Designer.cs
+++ Billing_System/Billing_System/motocarJobCard.Designer.cs
@@ -29,23 +29,26 @@ namespace Billing_System
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(motocarJobCard));
             this.panel1 = new System.Windows.Forms.Panel();
             this.btn_print = new Guna.UI2.WinForms.Guna2Button();
             this.printDialog1 = new System.Windows.Forms.PrintDialog();
             this.printDocument = new System.Drawing.Printing.PrintDocument();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.BackgroundImage = global::Billing_System.Properties.Resources.No_10__Galapitamulla__Hindagolla_076_1558383_auroraautocare1st_gmail_com_www_Aurora_autocare_facebook_com;
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
             this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.panel1.Location = new System.Drawing.Point(0, 48);
+            this.panel1.Location = new System.Drawing.Point(0, -46);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(526, 502);
+            this.panel1.Size = new System.Drawing.Size(842, 795);
             this.panel1.TabIndex = 38;
             // 
             // btn_print
             // 
+            this.btn_print.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btn_print.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
             this.btn_print.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
             this.btn_print.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
@@ -54,7 +57,7 @@ namespace Billing_System
             this.btn_print.ForeColor = System.Drawing.Color.White;
             this.btn_print.Location = new System.Drawing.Point(0, -3);
             this.btn_print.Name = "btn_print";
-            this.btn_print.Size = new System.Drawing.Size(526, 45);
+            this.btn_print.Size = new System.Drawing.Size(842, 45);
             this.btn_print.TabIndex = 39;
             this.btn_print.Text = "Print";
             this.btn_print.Click += new System.EventHandler(this.btn_print_Click);
@@ -67,17 +70,28 @@ namespace Billing_System
             // 
             this.printDocument.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.PrintPage);
             // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
             // motocarJobCard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(529, 547);
+            this.ClientSize = new System.Drawing.Size(841, 749);
             this.Controls.Add(this.btn_print);
             this.Controls.Add(this.panel1);
             this.DoubleBuffered = true;
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "motocarJobCard";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "motocarJobCard";
             this.Load += new System.EventHandler(this.motocarJobCard_Load);
             this.ResumeLayout(false);
@@ -89,5 +103,6 @@ namespace Billing_System
         private Guna.UI2.WinForms.Guna2Button btn_print;
         private System.Windows.Forms.PrintDialog printDialog1;
         private System.Drawing.Printing.PrintDocument printDocument;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
     }
 }
