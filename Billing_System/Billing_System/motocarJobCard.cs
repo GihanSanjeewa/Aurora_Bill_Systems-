f74df61911f629cf8fe1e569f43b8e2913162cd0
GihanSanjeewa <123794603+GihanSanjeewa@users.noreply.github.com>
2023-07-22 18:52:27 +0530

    job card car fully completed

--- Billing_System/Billing_System/motocarJobCard.cs
+++ Billing_System/Billing_System/motocarJobCard.cs
@@ -24,33 +24,34 @@ namespace Billing_System
 
         private void motocarJobCard_Load(object sender, EventArgs e)
         {
-            //lbl_customerName.Text = frm_motoCar.setCustomerName;
-            //lbl_vehicleModel.Text = frm_motoCar.setvehicleModel;
-            //lbl_dateReceived.Text = frm_motoCar.setdateReceived;
-            //lbl_time.Text = frm_motoCar.settime;
-            //lbl_fuelType.Text = frm_motoCar.setfuelType;
-            //lbl_address.Text = frm_motoCar.setaddress;
-            //lbl_regNo.Text = frm_motoCar.setregNo;
-            //lbl_OdoMeter.Text = frm_motoCar.setodoMeter;
-            //lbl_phoneNumber.Text = frm_motoCar.setphoneNumber;
-            //lbl_chassisNo.Text = frm_motoCar.setchassisNo;
-            //lbl_cName.Text = frm_motoCar.setcName;
-            //lbl_complaints1.Text = frm_motoCar.setcomplaints1;
-            //lbl_complaintsc2.Text = frm_motoCar.setcomplaints2;
-            //lbl_complaints3.Text = frm_motoCar.setcomplaints3;
-            //lbl_complaints4.Text = frm_motoCar.setcomplaints4;
-            //lbl_decision1.Text = frm_motoCar.setdecision1;
-            //lbl_decision2.Text = frm_motoCar.setdecision2;
-            //lbl_decision3.Text = frm_motoCar.setdecision3;
-            //lbl_decision4.Text = frm_motoCar.setdecision4;
-            //lbl_remarks.Text = frm_motoCar.setremark1;
-            //lbl_estimatedCost.Text = frm_motoCar.setestimatedCost;
-            //lblPdatec.Text = frm_motoCar.setpDate;
-            //lbl_dtimec.Text = frm_motoCar.setdtime;
-            //lbl_CInform.Text = frm_motoCar.setcInform;
-            //lbl_nDatec.Text = DateTime.Now.ToShortDateString();
+            lbl_customerName.Text = frm_motoCar.setCustomerName;
+            lbl_vehicleModel.Text = frm_motoCar.setvehicleModel;
+            lbl_dateReceived.Text = frm_motoCar.setdateReceived;
+            lbl_time.Text = frm_motoCar.settime;
+            lbl_fuelType.Text = frm_motoCar.setfuelType;
+            lbl_address.Text = frm_motoCar.setaddress;
+            lbl_regNo.Text = frm_motoCar.setregNo;
+            lbl_OdoMeter.Text = frm_motoCar.setodoMeter;
+            lbl_phoneNumber.Text = frm_motoCar.setphoneNumber;
+            lbl_chassisNo.Text = frm_motoCar.setchassisNo;
+            lbl_cName.Text = frm_motoCar.setcName;
+            lbl_complaints1.Text = frm_motoCar.setcomplaints1;
+            lbl_complaints2.Text = frm_motoCar.setcomplaints2;
+            lbl_complaints3.Text = frm_motoCar.setcomplaints3;
+            lbl_complaints4.Text = frm_motoCar.setcomplaints4;
+            lbl_decision1.Text = frm_motoCar.setdecision1;
+            lbl_decision2.Text = frm_motoCar.setdecision2;
+            lbl_decision3.Text = frm_motoCar.setdecision3;
+            lbl_decision4.Text = frm_motoCar.setdecision4;
+            lbl_remarks.Text = frm_motoCar.setremark1;
+            lbl_estimatedCost.Text = frm_motoCar.setestimatedCost;
+            lblPdatec.Text = frm_motoCar.setpDate;
+            lbl_dtimec.Text = frm_motoCar.setdtime;
+            lbl_CInform.Text = frm_motoCar.setcInform;
+        
+            lbl_ddate.Text = DateTime.Now.ToShortDateString();
 
-            panel1.BackgroundImage = Image.FromFile("C:\\Users\\Gihan Sanjeewa\\Documents\\GitHub\\Aurora_Bill_Systems-\\Billing_System\\Billing_System\\Resources\\No.10, Galapitamulla, Hindagolla 076-1558383 auroraautocare1st@gmail.com www.Aurora autocare.facebook.com.png");
+            //panel1.BackgroundImage = Image.FromFile("C:\\Users\\Gihan Sanjeewa\\Documents\\GitHub\\Aurora_Bill_Systems-\\Billing_System\\Billing_System\\Resources\\No.10, Galapitamulla, Hindagolla 076-1558383 auroraautocare1st@gmail.com www.Aurora autocare.facebook.com.png");
             panel1.BackgroundImageLayout = ImageLayout.Stretch;
 
 
