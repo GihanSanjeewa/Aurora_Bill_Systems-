62791f2dc2f4a61eb9430d43bc6bfc2bc60a8f01
Udara Ekanayake <113182571+udaraekanayake96@users.noreply.github.com>
2023-08-02 20:03:43 +0530

    Update console.js last

--- console.js
+++ console.js
@@ -1,7 +1,8 @@
 //this is a comment
 console.log("Hello Aurora")
 console.log("Hello Aurora Auto Service") // 2nd time 
-
+console.log("Hello Aurora Auto Service") // 2nd time 
+console.log("Hello Aurora Auto Service") // 2nd time 
 
 function main(){
   return null;
